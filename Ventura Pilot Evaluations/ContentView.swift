c356c6d4d88f4825bc9819007027e6206933a854
fredregis <51204669+fredregis@users.noreply.github.com>
2026-05-15 13:58:17 -0400

    Initial Commit

---
+++ Ventura Pilot Evaluations/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ventura Pilot Evaluations
+//
+//  Created by Frederic Regis on 5/15/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ventura Pilot Evaluations/Ventura_Pilot_EvaluationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ventura_Pilot_EvaluationsApp.swift
+//  Ventura Pilot Evaluations
+//
+//  Created by Frederic Regis on 5/15/26.
+//
+
+import SwiftUI
+
+@main
+struct Ventura_Pilot_EvaluationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
